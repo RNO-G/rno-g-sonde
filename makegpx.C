2431a1e743f9042824a0e321751878c744179177
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2023-08-23 13:04:51 -0500

    add support for json fragments

--- makegpx.C
+++ makegpx.C
@@ -1,4 +1,4 @@
-int makegpx(const char * rootfile, const char *gpxfile, const char * name = 0)
+int makegpx(const char * rootfile, const char *gpxfile, const char * name = 0, const char * json_fragment = 0)
 {
 
   TFile f(rootfile); 
@@ -34,6 +34,8 @@ int makegpx(const char * rootfile, const char *gpxfile, const char * name = 0)
 
 
 
+  t->GetEntry(0); 
+  double start_time = tim; 
   for (int i = 0; i < t->GetEntries(); i++)
   {
     t->GetEntry(i); 
@@ -49,6 +51,22 @@ int makegpx(const char * rootfile, const char *gpxfile, const char * name = 0)
   fprintf(gpx,"  </trk>\n"); 
   fprintf(gpx,"</gpx>\n"); 
   fclose(gpx); 
+
+  if (json_fragment) 
+  {
+    printf("Writing json fragment to %s\n", json_fragment); 
+    FILE * json = fopen(json_fragment,"w"); 
+    fprintf(json,"  {\n"); 
+    fprintf(json, "  \"filename\": \"%s\",\n", rootfile); 
+    fprintf(json, "  \"name\": \"%s\",\n", name ?: rootfile); 
+    fprintf(json, "  \"start_time\": %f,\n", start_time ); 
+    fprintf(json, "  \"end_time\": %f,\n", tim ); 
+    fprintf(json, "  \"end_lat\": %f,\n", lat ); 
+    fprintf(json, "  \"end_lon\": %f,\n", lon ); 
+    fprintf(json, "  \"end_alt\": %f\n", alt ); 
+    fprintf(json,"  }\n"); 
+    fclose(json); 
+  }
   return 0; 
 }
 
